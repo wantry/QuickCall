b2154053b2d05d06bbab83a8739e708e6c8b8e12
吴挺 <399798188@qq.com>
2014-09-27 08:33:58 +0800

    通知栏添加五个项目

--- quickSettings/TodayViewController.m
+++ quickSettings/TodayViewController.m
@@ -18,10 +18,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    NSLog(@"viewDidLoad");
+
+    UIButton *btn_data=[UIButton buttonWithType:UIButtonTypeCustom];
+    [btn_data addTarget:self action:@selector(btn_quickData:) forControlEvents:UIControlEventTouchUpInside];
+    [btn_data setTitle:@"蜂窝" forState:UIControlStateNormal];
+    [btn_data setFrame:CGRectMake([self x:0], 10, 60, 60)];
+    [self.view addSubview:btn_data];
     
-    UIView *view=[[UIView alloc]initWithFrame:CGRectMake(0, 0, 100, 100)];
-    [view setBackgroundColor:[UIColor redColor]];
-    [self.view addSubview:view];
+    
+    UIButton *btn_location=[UIButton buttonWithType:UIButtonTypeCustom];
+    [btn_location addTarget:self action:@selector(btn_quickLocation:) forControlEvents:UIControlEventTouchUpInside];
+    [btn_location setTitle:@"位置" forState:UIControlStateNormal];
+    [btn_location setFrame:CGRectMake([self x:1], 10, 60, 60)];
+    [self.view addSubview:btn_location];
+
+    UIButton *btn_notification=[UIButton buttonWithType:UIButtonTypeCustom];
+    [btn_notification addTarget:self action:@selector(btn_quickNotification:) forControlEvents:UIControlEventTouchUpInside];
+    [btn_notification setTitle:@"通知" forState:UIControlStateNormal];
+    [btn_notification setFrame:CGRectMake([self x:2], 10, 60, 60)];
+    [self.view addSubview:btn_notification];
+    
+    UIButton *btn_hot=[UIButton buttonWithType:UIButtonTypeCustom];
+    [btn_hot addTarget:self action:@selector(btn_quickHotPoint:) forControlEvents:UIControlEventTouchUpInside];
+    [btn_hot setTitle:@"热点" forState:UIControlStateNormal];
+    [btn_hot setFrame:CGRectMake([self x:3], 10, 60, 60)];
+    [self.view addSubview:btn_hot];
+
+    
+    UIButton *btn_vpn=[UIButton buttonWithType:UIButtonTypeCustom];
+    [btn_vpn addTarget:self action:@selector(btn_quickVPN:) forControlEvents:UIControlEventTouchUpInside];
+    [btn_vpn setTitle:@"VPN" forState:UIControlStateNormal];
+    [btn_vpn setFrame:CGRectMake([self x:4], 10, 60, 60)];
+    [self.view addSubview:btn_vpn];
+
+    
     
     
     CGSize updatedSize = [self preferredContentSize];
@@ -47,10 +78,42 @@
 }
 - (UIEdgeInsets)widgetMarginInsetsForProposedMarginInsets:(UIEdgeInsets)defaultMarginInsets
 {
-
-    
     return UIEdgeInsetsMake(0, 0, 0, 0);
+}
+
+
+-(IBAction)btn_quickLocation:(id)sender
+{
+    NSURL *url=[NSURL URLWithString:@"prefs:root=LOCATION_SERVICES"];
+    [self.extensionContext openURL:url completionHandler:nil];
+}
+-(IBAction)btn_quickData:(id)sender
+{
+    NSURL *url=[NSURL URLWithString:@"prefs:root=MOBILE_DATA_SETTINGS_ID"];
+    [self.extensionContext openURL:url completionHandler:nil];
+}
+-(IBAction)btn_quickVPN:(id)sender
+{
+    NSURL *url=[NSURL URLWithString:@"prefs:root=General&path=Network/VPN"];
+    [self.extensionContext openURL:url completionHandler:nil];
+    
+}
+-(IBAction)btn_quickNotification:(id)sender
+{
+    NSURL *url=[NSURL URLWithString:@"prefs:root=NOTIFICATIONS_ID"];
+    [self.extensionContext openURL:url completionHandler:nil];
+}
+-(IBAction)btn_quickHotPoint:(id)sender
+{
+    NSURL *url=[NSURL URLWithString:@"prefs:root=INTERNET_TETHERING"];
+    [self.extensionContext openURL:url completionHandler:nil];
+}
+
+-(NSInteger)x:(NSInteger)i
+{
+    return i*(50+5)+10;
 
 }
+
 
 @end
